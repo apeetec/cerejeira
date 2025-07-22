d67e99645610330db2f00313d66546bc047797f3
APEETEC <gabriel938@gmail.com>
2025-07-22 01:15:03 -0300

    Configurando o vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/cerejeira/', // <- nome exato do repositório no GitHub
   plugins: [react()],
 })
